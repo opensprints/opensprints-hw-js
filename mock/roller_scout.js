246b346fc48417c2f88c392b29f7922f0898f6af
Luke Orland <orluke@gmail.com>
2017-01-18 23:26:21 -0500

    WIP

--- mock/roller_scout.js
+++ mock/roller_scout.js
@@ -1,41 +1,54 @@
 var EventEmitter = require('events');
 var Scout = require("zetta-scout");
 var util = require("util");
-var Roller = require("./roller_device");
+var Roller = require("../roller_device");
 
 var LED_PINS = [9, 10, 11, 12];
 var SENSOR_PINS = [2, 3, 4, 5];
 
-// get a random string and emit the appropriate event every 3 seconds
-// https://coligo.io/nodejs-event-emitter/
-function MockButton() {
-  EventEmitter.call(this)
 
-  const that = this
-  setInterval(function () {
-    var randomStr = swapState()
-
-    if (randomStr.indexOf('coligo') > -1) {
-      that.emit('coligo')
-    } else if (randomStr.indexOf('nodejs') > -1) {
-      that.emit('nodejs')
-    } else if (randomStr.indexOf('javascript') > -1) {
-      that.emit('javascript')
-    }
-  }, 3000) // TOOD: random amount of time
-};
-
-// conventionally, NodeJs uses util.inherits() for inheritance
-util.inherits(MockButton, EventEmitter)
-
-// simple function to randomly select a string from an array
-function swapState () {
-  const stringsArr = ['nodejs', 'coligo', 'javascript', 'EventEmitters',
-                      'random text', 'testing events in node', 'MockButton and events at coligo']
-  return stringsArr[Math.floor(Math.random() * stringsArr.length)]
+function random(low, high) {
+  return Math.random() * (high - low) + low;
 }
 
-module.exports = MockButton
+function swapState (state) {
+  return !state;
+}
+
+// https://coligo.io/nodejs-event-emitter/
+var MockButton = function(name) {
+  EventEmitter.call(this);
+
+  const that = this;
+  var buttonState = false;
+
+  setInterval(function () {
+    buttonState = swapState(buttonState);
+    if (buttonState) {
+      console.log("mock button " + name + " down");
+      that.emit("down");
+    } else {
+      console.log("mock button " + name + " up");
+      that.emit("up");
+    }
+  }, random(250, 1500)); // TOOD: random amount of time
+};
+util.inherits(MockButton, EventEmitter);
+
+var MockLed = function(name) {
+  var self = this;
+  self.name = name;
+}
+
+MockLed.prototype.off = function() {
+  var self = this;
+  console.log("mock LED " + self.name + " off");
+};
+
+MockLed.prototype.on = function() {
+  var self = this;
+  console.log("mock LED " + self.name + " on");
+};
 
 var j5Scout = module.exports = function() {
   Scout.call(this);
@@ -45,24 +58,22 @@ util.inherits(j5Scout, Scout);
 
 j5Scout.prototype.init = function(next) {
   var self = this;
-  board.on("ready", function() {
 
-    var leds = LED_PINS.map(function(pin) {
-      return false;
-    });
-
-    var sensors = SENSOR_PINS.map(function(pin) {
-      return false;
-    });
-
-    for(var i = 0; i < SENSOR_PINS.length; i++) {
-      var name = "roller_" + i;
-      console.log(name);
-      self.discover(Roller, leds[i], sensors[i], name);
-    }
-
-    if(next) {
-      next();
-    }
+  var leds = LED_PINS.map(function(pin) {
+    return MockLed(pin);
   });
+
+  var sensors = SENSOR_PINS.map(function(pin) {
+    return MockButton(pin);
+  });
+
+  for(var i = 0; i < SENSOR_PINS.length; i++) {
+    var name = "roller_" + i;
+    console.log(name);
+    self.discover(Roller, leds[i], sensors[i], name);
+  }
+
+  if(next) {
+    next();
+  }
 };
